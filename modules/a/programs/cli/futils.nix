2150f962c2e882e140e30ae1855aa7efc9842f90
Daniel Donohue <d.michael.donohue@gmail.com>
2026-04-29 18:14:11 -0400

    ouch...
    Is a cli that is better than what I've been carrying all this time.

--- modules/a/programs/cli/futils.nix
+++ modules/a/programs/cli/futils.nix
@@ -5,35 +5,12 @@
   # File CLI utils.
   a.futils.homeManager =
     { pkgs, ... }:
-    let
-      extractPkg =
-        {
-          bzip3,
-          gzip,
-          gnutar,
-          p7zip,
-          writeShellApplication,
-        }:
-        writeShellApplication {
-          name = "extract";
-          runtimeInputs = [
-            bzip3
-            gzip
-            gnutar
-            p7zip
-          ];
-          text = builtins.readFile ./scripts/extract.sh;
-        };
-    in
     {
       home.packages = with pkgs; [
-        (callPackage extractPkg { })
+        ouch
         parquet-tools
         snappy
         tree-sitter
-        unzipNLS
-        zip
-        zstd
       ];
     };
 }
