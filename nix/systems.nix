66c394d4ffa9e770780cf9a0ba65c039e558cbfc
Daniel Donohue <d.michael.donohue@gmail.com>
2026-09-16 22:23:43 -0400

    so close i can feel it

--- nix/systems.nix
+++ nix/systems.nix
@@ -1,8 +1,11 @@
 {
-  lib,
-  den,
-  ...
-}:
-{
-  systems = lib.attrNames den.hosts;
+  # Very slick, @vic, but we're doing stuff on linux runners in CI/CD and don't
+  # have any nixos hosts atm.
+  # systems = lib.attrNames den.hosts;
+
+  systems = [
+    "aarch64-darwin"
+    "aarch64-linux"
+    "x86_64-linux"
+  ];
 }
